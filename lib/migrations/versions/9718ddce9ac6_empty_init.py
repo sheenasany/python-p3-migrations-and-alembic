4a9d5951dca73c18cd600511e3a3f87a45a9f788
Sheena Sang <sheenasany@gmail.com>
2023-03-01 14:58:56 -0500

    completed lab

---
+++ lib/migrations/versions/9718ddce9ac6_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 9718ddce9ac6
+Revises: 
+Create Date: 2023-03-01 14:14:38.954858
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9718ddce9ac6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
